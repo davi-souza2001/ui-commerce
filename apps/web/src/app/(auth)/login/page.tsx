75d74f73e6003480cd5a6e0a916aebc6086715d2
Davi Souza <davisouza2001dv@gmail.com>
2024-01-14 08:56:00 -0300

    feat: add hook form

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,21 +1,59 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const createLoginFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'E-mail é obrigatório!')
+    .email('Formato de e-mail inválido!')
+    .toLowerCase(),
+  password: z.string().min(4, 'A senha está incorreta!'),
+})
+
+type CreateLoginFormData = z.infer<typeof createLoginFormSchema>
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateLoginFormData>({
+    resolver: zodResolver(createLoginFormSchema),
+  })
+
+  const handleLogin = async (data: CreateLoginFormData) => {
+    console.log(data)
+  }
+
   return (
     <main className="min-h-screen w-full flex items-center justify-center bg-gradient-to-l from-red-600 to-red-900">
       <div className="shadow-lg p-14 bg-white rounded-md">
         <h1 className="text-3xl font-bold mb-8">Login</h1>
-        <form className="flex flex-col gap-3">
+        <form
+          className="flex flex-col gap-3"
+          onSubmit={handleSubmit(handleLogin)}
+        >
           <input
-            type="text"
-            placeholder="Username"
+            type="email"
+            placeholder="E-mail"
             className="w-60 border border-gray-300 p-2 mb-4"
+            {...register('email')}
           />
+          {errors?.email?.message && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
           <input
             type="password"
             placeholder="Password"
             className="border border-gray-300 p-2 mb-2"
+            {...register('password')}
           />
+          {errors?.password?.message && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
           <Link href="/register" className="mb-2 ">
             <p className="cursor-pointer transition-all hover:text-gray-600">
               Register
--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -1,26 +1,69 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const createRegisterFormSchema = z.object({
+  name: z.string().min(1, 'Nome é obrigatório!'),
+  email: z
+    .string()
+    .min(1, 'E-mail é obrigatório!')
+    .email('Formato de e-mail inválido!')
+    .toLowerCase(),
+  password: z.string().min(4, 'A senha precisa ter no mínimo 4 caracteres!'),
+})
+
+type CreateRegisterFormData = z.infer<typeof createRegisterFormSchema>
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateRegisterFormData>({
+    resolver: zodResolver(createRegisterFormSchema),
+  })
+
+  const handleRegister = async (data: CreateRegisterFormData) => {
+    console.log(data)
+  }
+
   return (
     <main className="min-h-screen w-full flex items-center justify-center bg-gradient-to-l from-red-600 to-red-900">
       <div className="shadow-lg p-14 bg-white rounded-md">
         <h1 className="text-3xl font-bold mb-8">Register</h1>
-        <form className="flex flex-col gap-3">
+        <form
+          className="flex flex-col gap-3"
+          onSubmit={handleSubmit(handleRegister)}
+        >
           <input
             type="text"
             placeholder="Username"
             className="w-60 border border-gray-300 p-2 mb-4"
+            {...register('name')}
           />
+          {errors?.name?.message && (
+            <p className="text-red-500">{errors.name.message}</p>
+          )}
           <input
-            type="password"
+            type="email"
             placeholder="Email"
             className="border border-gray-300 p-2 mb-2"
+            {...register('email')}
           />
+          {errors?.email?.message && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
           <input
             type="password"
             placeholder="Password"
             className="border border-gray-300 p-2 mb-2"
+            {...register('password')}
           />
+          {errors?.password?.message && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
           <Link href="/login" className="mb-2 ">
             <p className="cursor-pointer transition-all hover:text-gray-600">
               Login
